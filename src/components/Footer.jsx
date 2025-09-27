d500df9aa9e3b443420adc7d829625ca898339d9
NuonThanith <163116657+nounthanith@users.noreply.github.com>
2025-09-27 13:41:03 +0700

    skip footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-gray-900 text-gray-300 border-t border-gray-800 ">
-      <h1 className="text-center p-4">Footer@{year}</h1>
-    </footer>
+    // <footer className="bg-gray-900 text-gray-300 border-t border-gray-800 ">
+    //   <h1 className="text-center p-4">Footer@{year}</h1>
+    // </footer>
+    <></>
   );
 }
 
